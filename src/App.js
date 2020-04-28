19b7a3ad0c8b45fb5b540c38273f4cdee4218c4b
SRattacasa <38533+SRattacasa@users.noreply.github.com>
2020-04-28 11:59:13 -0400

    Axios added and basic API calls made

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  const axios = require('axios');
+  const apiURL = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`;
+  // ${date-yyyy-mm-dd}/${hd}
+  axios.get(apiURL)
+  .then(response => {console.log(response)})
+  .catch(error => {console.log(error)})
   return (
     <div className="App">
+      
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
