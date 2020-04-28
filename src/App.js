b86d50d72ac1c5937f2d3c31a89d964c25db5e9c
SRattacasa <38533+SRattacasa@users.noreply.github.com>
2020-04-28 19:28:38 -0400

    Added the second component and passed it props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import NasaImage from "./components/NasaImage";
+import HeaderComp from "./components/HeaderComp"
 
 function App() {
   
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <div className="App">
+      <HeaderComp headline="My NASA Birthday Photo is shown below" />
       <NasaImage />
       <p>
         Read through the instructions in the README.md file to build your NASA
---
+++ src/components/HeaderComp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeaderComp = (props) => {
+    return (
+        <div>
+            <h1>{props.headline}</h1>
+        </div>
+    )
+};
+
+export default HeaderComp;
